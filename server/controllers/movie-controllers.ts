c75c99e52f887d6691e377e11b88a6df5ab05308
Justin OConnell <itsjustinoconnell@gmail.com>
2024-07-25 11:50:34 +0100

    error handling fixed

--- server/controllers/movie-controllers.ts
+++ server/controllers/movie-controllers.ts
@@ -13,8 +13,8 @@ export async function getTrendingMovie(req: Request, res: Response) {
     res.json({ success: true, content: randomMovie });
   } catch (err) {
     if (err instanceof AxiosError) {
-      if (err.status === 404) {
-        return res.status(404).send(null);
+      if (err.message.includes("404")) {
+        return res.status(404).json({ message: "resource not found" });
       }
     }
     res.status(500).json({ success: false, message: "Internal Server Error" });
@@ -30,9 +30,10 @@ export async function getMovieIdTrailers(req: Request, res: Response) {
 
     res.json({ success: true, content: data.results });
   } catch (err) {
+    console.log(err);
     if (err instanceof AxiosError) {
-      if (err.status === 404) {
-        return res.status(404).send(null);
+      if (err.message.includes("404")) {
+        return res.status(404).json({ message: "resource not found" });
       }
     }
     res.status(500).json({ success: false, message: "Internal Server Error" });
@@ -46,8 +47,8 @@ export async function getMovieIdDetails(req: Request, res: Response) {
     res.json({ success: true, content: data });
   } catch (err) {
     if (err instanceof AxiosError) {
-      if (err.status === 404) {
-        return res.status(404).send(null);
+      if (err.message.includes("404")) {
+        return res.status(404).json({ message: "resource not found" });
       }
     }
     res.status(500).json({ success: false, message: "Internal Server Error" });
