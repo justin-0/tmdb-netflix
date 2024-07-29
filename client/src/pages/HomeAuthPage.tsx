415ffff0d693ca180b1941f4ff94237d516f600b
Justin OConnell <itsjustinoconnell@gmail.com>
2024-07-29 14:03:50 +0100

    pages for auth user and new user, styled homepage

---
+++ client/src/pages/HomeAuthPage.tsx
@@ -0,0 +1,86 @@
+import { Link } from "react-router-dom";
+import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { ChevronRight } from "lucide-react";
+
+function HomeAuthPage() {
+  return (
+    <div>
+      <div className="min-h-[30rem] w-full bg-hero-img md:min-h-[40rem]">
+        <header className="px-6 py-3 md:px-24">
+          <nav className="flex items-center justify-between">
+            <Link to={"/"}>
+              <img
+                src="/netflix-logo.png"
+                alt="neflix company logo"
+                className="w-24 md:w-32"
+              />
+            </Link>
+            <div className="hidden md:flex">
+              <Link to={"/login"}>
+                <Button className="bg-netflix text-white hover:bg-netflix/80 hover:text-white/80">
+                  Login
+                </Button>
+              </Link>
+            </div>
+            <div className="md:hidden">
+              <Link to={"/login"}>
+                <Button
+                  size="sm"
+                  className="bg-netflix text-white hover:bg-netflix/80 hover:text-white/80"
+                >
+                  Login
+                </Button>
+              </Link>
+            </div>
+          </nav>
+        </header>
+        <div className="mt-20 md:mt-40">
+          <section className="flex flex-col items-center justify-center space-y-4 text-white">
+            <h1 className="text-balance text-center text-3xl font-bold md:text-5xl">
+              Unlimited films, TV programmes and more
+            </h1>
+            <p className="text-center text-lg lg:text-2xl">
+              Watch anywhere. Cancel anytime.
+            </p>
+            <p className="text-balance text-center text-lg lg:text-2xl">
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </p>
+            <div className="flex w-3/4 flex-col items-center justify-center gap-y-4 md:flex-row md:gap-x-2">
+              <Input
+                className="w-full md:w-[400px]"
+                placeholder="Email address"
+              />
+              <div className="flex items-center justify-center">
+                <Button
+                  className="bg-netflix font-bold text-white hover:bg-netflix/80 hover:text-white/80 md:text-xl"
+                  size="lg"
+                >
+                  Get Started
+                  <ChevronRight className="ml-2 h-6 w-6" />
+                </Button>
+              </div>
+            </div>
+          </section>
+        </div>
+      </div>
+      <div className="flex min-h-[500px] flex-col items-center justify-center bg-black">
+        <div className="mx-auto flex flex-col items-center justify-center outline outline-red-500 md:w-2/4 md:flex-row">
+          <div className="flex flex-col justify-center md:w-2/4 md:items-start">
+            <h2 className="text-center text-3xl font-bold text-white md:text-left md:text-5xl">
+              Enjoy on your TV
+            </h2>
+            <p className="mt-4 text-balance text-center text-lg text-white md:text-left lg:text-2xl">
+              Watch on smart TVs, PlayStation, Xbox, Chromecast, Apple TV,
+              Blu-ray players and more.
+            </p>
+          </div>
+          <div className="w-2/4 text-white">photo frame</div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomeAuthPage;
--- client/src/routes/App.tsx
+++ client/src/routes/App.tsx
@@ -1,72 +1,10 @@
-import { Link } from "react-router-dom";
-import { Button } from "../components/ui/button";
-import { Input } from "../components/ui/input";
-import { ChevronRight } from "lucide-react";
+import HomeAuthPage from "../pages/HomeAuthPage";
+import HomePage from "../pages/HomePage";
+
+const user = false;
 
 function App() {
-  return (
-    <div>
-      <div className="min-h-[30rem] w-full bg-hero-img md:min-h-[40rem]">
-        <header className="px-6 py-3 md:px-24">
-          <nav className="flex items-center justify-between">
-            <Link to={"/"}>
-              <img
-                src="/netflix-logo.png"
-                alt="neflix company logo"
-                className="w-24 md:w-32"
-              />
-            </Link>
-            <div className="hidden md:flex">
-              <Link to={"/login"}>
-                <Button className="bg-netflix text-white hover:bg-netflix/80 hover:text-white/80">
-                  Login
-                </Button>
-              </Link>
-            </div>
-            <div className="md:hidden">
-              <Link to={"/login"}>
-                <Button
-                  size="sm"
-                  className="bg-netflix text-white hover:bg-netflix/80 hover:text-white/80"
-                >
-                  Login
-                </Button>
-              </Link>
-            </div>
-          </nav>
-        </header>
-        <div className="mt-20 md:mt-40">
-          <section className="flex flex-col items-center justify-center space-y-4 text-white">
-            <h1 className="text-balance text-center text-3xl font-bold md:text-5xl">
-              Unlimited films, TV programmes and more
-            </h1>
-            <p className="text-center text-lg lg:text-2xl">
-              Watch anywhere. Cancel anytime.
-            </p>
-            <p className="text-balance text-center text-lg lg:text-2xl">
-              Ready to watch? Enter your email to create or restart your
-              membership.
-            </p>
-            <div className="flex w-3/4 flex-col items-center justify-center gap-y-4 md:flex-row md:gap-x-2">
-              <Input
-                className="w-full md:w-[400px]"
-                placeholder="Email address"
-              />
-              <div className="flex items-center justify-center">
-                <Button
-                  className="bg-netflix font-bold text-white hover:bg-netflix/80 hover:text-white/80 md:text-xl"
-                  size="lg"
-                >
-                  Get Started
-                  <ChevronRight className="ml-2 h-6 w-6" />
-                </Button>
-              </div>
-            </div>
-          </section>
-        </div>
-      </div>
-    </div>
-  );
+  return <div>{user ? <HomePage /> : <HomeAuthPage />}</div>;
 }
 
 export default App;
