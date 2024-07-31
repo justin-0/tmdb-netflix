17ac4576093b7e9affc6d1cd2e13ef643733c227
Justin OConnell <itsjustinoconnell@gmail.com>
2024-07-31 10:31:29 +0100

    homepage hero movie info

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,6 @@
+import { Info, Play } from "lucide-react";
 import Navbar from "../components/navbars/auth-nav";
+import { Button } from "../components/ui/button";
 
 function HomePage() {
   return (
@@ -12,6 +14,31 @@ function HomePage() {
       />
 
       <div className="absolute left-0 top-0 -z-50 h-screen w-full bg-gradient-to-b from-black to-transparent" />
+      <div className="mt-52 max-w-2xl px-6 text-white outline md:px-24">
+        <div>
+          {/* title */}
+          <h1 className="text-4xl font-bold md:text-6xl">Extraction</h1>
+          {/* rating */}
+          <p className="mt-2 text-sm">18+ | 2014</p>
+          {/* description */}
+          <p className="mt-2">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Asperiores, officiis.
+          </p>
+        </div>
+        <div className="mt-4 space-x-6">
+          {/* TODO: LINK TO WATCH ID PATH */}
+          <Button>
+            <Play className="mr-2 h-4 w-4" />
+            Play
+          </Button>
+          {/* TODO: LINK TO MOVIE DETAILS ID PATH */}
+          <Button variant="secondary">
+            <Info className="mr-2 h-4 w-4" />
+            More Info
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
