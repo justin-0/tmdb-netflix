b10526bfb927165e40bfeb33fdd3213d99462c23
Justin OConnell <itsjustinoconnell@gmail.com>
2024-08-01 14:21:17 +0100

    slider container sitting below content

--- client/src/components/slider.tsx
+++ client/src/components/slider.tsx
@@ -6,7 +6,7 @@ type SliderProps = {
 
 function Slider({ data }: SliderProps) {
   return (
-    <div>
+    <div className="relative bg-black px-5 text-white md:px-20">
       {data.map((d) => (
         <h1 className="text-white">{d.title || d.name}</h1>
       ))}
--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -44,23 +44,24 @@ function HomePage() {
   }
   // console.log(categories);
   return (
-    <div className="relative h-full w-full">
-      <Navbar />
+    <>
+      <div className="relative h-screen w-full">
+        <Navbar />
 
-      <img
-        src={`https://image.tmdb.org/t/p/original/${data.backdrop_path}`}
-        alt="name of file poster"
-        className="absolute left-0 top-0 -z-50 h-screen w-full object-cover md:-translate-x-0"
-      />
+        <img
+          src={`https://image.tmdb.org/t/p/original/${data.backdrop_path}`}
+          alt="name of file poster"
+          className="absolute left-0 top-0 -z-50 h-full w-full object-cover"
+        />
 
-      <div className="absolute left-0 top-0 -z-50 h-screen w-full bg-gradient-to-b from-black to-transparent" />
+        <div className="absolute left-0 top-0 -z-50 h-screen w-full bg-gradient-to-b from-black to-transparent" />
 
-      <ContentDisplay data={data} content={content} />
-
+        <ContentDisplay data={data} content={content} />
+      </div>
       <div className="flex flex-col gap-10 bg-black py-10">
         {categories?.map((cat) => <Slider data={cat} />)}
       </div>
-    </div>
+    </>
   );
 }
 
