a1965d87197907ee47b9a37b1504439d26edde33
Justin OConnell <itsjustinoconnell@gmail.com>
2024-08-02 15:23:19 +0100

    Watch route, video player renders current video, trailers controlled with buttons

--- client/src/routes/Watch.tsx
+++ client/src/routes/Watch.tsx
@@ -4,6 +4,14 @@ import useAuthStore from "../store/auth-store";
 import { useNavigate, useParams } from "react-router-dom";
 import useMediaStore from "../store/media-store";
 import axios from "axios";
+import ReactPlayer from "react-player";
+import { Button } from "../components/ui/button";
+import { ChevronLeft, ChevronRight } from "lucide-react";
+
+type TrailerType = {
+  key: string;
+  name: string;
+};
 
 function WatchRoute() {
   const { user } = useAuthStore();
@@ -11,10 +19,13 @@ function WatchRoute() {
   const navigate = useNavigate();
   const params = useParams();
 
-  const [trailers, setTrailers] = useState<[]>();
+  const [trailers, setTrailers] = useState<TrailerType[]>([]);
   const [similarMedia, setSimilarMedia] = useState<[]>();
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const [media, setMedia] = useState({});
+  const startOfTrailers = currentIndex === 0 ? true : false;
+  const endOfTrailers = currentIndex === trailers.length - 1;
+  console.log(currentIndex === 0);
 
   useEffect(() => {
     if (!user) {
@@ -42,7 +53,7 @@ function WatchRoute() {
     const getTrailers = async () => {
       try {
         const response = await axios.get(
-          `/api/v1/${content}/${params.id}/similar`,
+          `/api/v1/${content}/${params.id}/trailers`,
         );
         setTrailers(response.data.content);
       } catch (err) {
@@ -73,9 +84,50 @@ function WatchRoute() {
     getMediaContent();
   }, [content, params.id]);
 
+  if (!trailers) {
+    return (
+      <div className="h-screen w-full bg-black text-white">
+        <Navbar />
+        <div className="mx-auto max-w-4xl outline outline-red-500">
+          {/* SHOW NO CONTENT STATE */}
+        </div>
+      </div>
+    );
+  }
+  // console.log(trailers);
   return (
     <div className="h-screen w-full bg-black text-white">
       <Navbar />
+      <div className="mx-auto flex w-10/12 flex-col items-center justify-center outline outline-red-500 md:max-w-4xl">
+        {/* SHOW CURRENT TRAILER */}
+        {trailers[currentIndex] ? (
+          <>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=${trailers[currentIndex].key}`}
+              controls={true}
+              width={"100%"}
+              height={"70vh"}
+              // className="h-full w-full"
+            />
+          </>
+        ) : null}
+        <div className="mt-4 space-x-8">
+          <Button
+            size="icon"
+            onClick={() => setCurrentIndex((i) => i - 1)}
+            disabled={startOfTrailers}
+          >
+            <ChevronLeft className="h-4 w-4" />
+          </Button>
+          <Button
+            size="icon"
+            onClick={() => setCurrentIndex((i) => i + 1)}
+            disabled={endOfTrailers}
+          >
+            <ChevronRight className="h-4 w-4" />
+          </Button>
+        </div>
+      </div>
     </div>
   );
 }
